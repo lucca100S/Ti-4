cffce747bed4993dbe66e40cf324500cc29259bc
LuguGMD <lugo.bhz@gmail.com>
2025-09-18 10:01:01 -0300

    fix - Removed movement system

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -1,38 +1,38 @@
-using Player.Movement;
+//using Player.Movement;
 using UnityEngine;
 
 namespace Player
 {
-    [RequireComponent(typeof(PlayerMovement), typeof(PlayerInput))]
+    [RequireComponent(typeof(PlayerInput))]
     public class PlayerController : MonoBehaviour
     {
 
-        private PlayerMovement _playerMovement;
+        //private PlayerMovement _playerMovement;
         private PlayerInput _playerInput;
 
         #region Properties
 
-        public PlayerMovement PlayerMovement { get => _playerMovement; private set => _playerMovement = value; }
+        //public PlayerMovement PlayerMovement { get => _playerMovement; private set => _playerMovement = value; }
         public PlayerInput PlayerInput { get => _playerInput; private set => _playerInput = value; }
 
         #endregion
 
         private void Awake()
         {
-            _playerMovement = GetComponent<PlayerMovement>();
+            //_playerMovement = GetComponent<PlayerMovement>();
             _playerInput = GetComponent<PlayerInput>();
         }
 
         private void OnEnable()
         {
-            _playerInput.OnMove += _playerMovement.ChangeDirection;
-            _playerInput.OnJump += _playerMovement.Jump;
+            //_playerInput.OnMove += _playerMovement.ChangeDirection;
+            //_playerInput.OnJump += _playerMovement.Jump;
         }
 
         private void OnDisable()
         {
-            _playerInput.OnMove -= _playerMovement.ChangeDirection;
-            _playerInput.OnJump -= _playerMovement.Jump;
+            //_playerInput.OnMove -= _playerMovement.ChangeDirection;
+            //_playerInput.OnJump -= _playerMovement.Jump;
         }
 
     }
