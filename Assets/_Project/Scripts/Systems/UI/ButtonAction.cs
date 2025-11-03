dc38353188c7921108b34b82807aa400dcab0f7d
Arthur Roque <arthur.roque.pro@gmail.com>
2025-11-03 13:47:14 -0300

    Event-driven UIAction

---
+++ Assets/_Project/Scripts/Systems/UI/ButtonAction.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class ButtonAction : ScriptableObject
+{
+    public abstract void Execute();
+}
--- Assets/_Project/Scripts/Systems/UI/PanelNames.cs
+++ Assets/_Project/Scripts/Systems/UI/PanelNames.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public enum PanelNames
 {
-    Settings, Collectables, Credits, Defeated, Loading, PUC_Logo, Main_Menu, Pause, Saving, HUD
+    Settings, Collectables, Credits, Defeated, Loading, PUC_Logo, Main_Menu, Pause, Saving, HUD, Error
 }
 
 
@@ -12,7 +13,25 @@ public static class PanelNamesExtensions
     //Pattern: PanelNames_Panel
     public static string ToPanelName(this PanelNames panelName)
     {
+        if (panelName == PanelNames.Error)
+        {
+            Debug.LogError($"Function called with {panelName}");
+            return string.Empty;
+        }
         return panelName.ToString() + "_Panel";
+    }
+
+    public static PanelNames ToPanelName(string panelName) 
+    {
+        string panelNameString = panelName.Split('_')[0];
+        foreach(PanelNames panelNames in Enum.GetValues(typeof(PanelNames)))
+        { 
+            if(panelNameString.CompareTo(panelNames.ToString()) == 0)
+            {
+                return panelNames;                
+            }
+        }
+        return PanelNames.Error;
     }
 }
 
