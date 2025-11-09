440ca1df2428fc99a8b7cf6115c24448afc22616
LuguGMD <lugo.bhz@gmail.com>
2025-11-08 21:06:35 -0300

    feat - Collectables

--- Assets/_Project/Scripts/Systems/Interactable Objects/Collectables/Collectables.cs
+++ Assets/_Project/Scripts/Systems/Interactable Objects/Collectables/Collectables.cs
@@ -1,4 +1,6 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
 /// Representa um coletável no cenário.
@@ -6,10 +8,41 @@ using UnityEngine;
 /// </summary>
 public class Collectables : OptionalInteractableObjects
 {
+    [SerializeField] private float _spinDuration = 1;
+    [SerializeField] private float _bounceHeight = 0.5f;
+    [SerializeField] private float _bounceDuration = 1;
+    [SerializeField] private ParticleSystem _collectEffect;
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Collider _collider;
+
+    private void Start()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), _spinDuration, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear);
+        transform.DOMoveY(transform.position.y + _bounceHeight, _bounceDuration)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
+    }
+
     public override void Interaction()
     {
         Debug.Log($"[Collectable] Coletado: {this.gameObject.name}");
         CollectableObservable.Instance?.NotifyListeners(this);
-        this.gameObject.SetActive(false);
+        _collider.enabled = false;
+        _renderer.enabled = false;
+        _collectEffect.Play();
+
+        transform.DOKill();
+
+        Destroy(this.gameObject, 2f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Interaction();
+        }
     }
 }
