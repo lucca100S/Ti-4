7b4d49280a4125684d22a96a360920b730484619
LuguGMD <lugo.bhz@gmail.com>
2025-11-19 10:03:09 -0300

    feat - Added second KillZone

---
+++ Assets/_Project/Scripts/Systems/Poison/PoisonCleanMaterial.cs
@@ -0,0 +1,35 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonCleanMaterial : MonoBehaviour
+{
+    private Renderer m_renderer;
+
+    private List<Material> m_materials = new List<Material>();
+
+    [SerializeField] private float _cleanDuration = 1f;
+
+    private void Awake()
+    {
+        m_renderer = GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < m_renderer.materials.Length; i++)
+        {
+            m_materials.Add(m_renderer.materials[i]);
+        }
+    }
+
+    public void Clean()
+    {
+        for(int i = 0;i < m_materials.Count;i++)
+        {
+            Material mat = m_materials[i];
+            mat.DOFloat(1, "_MaterialBlend", _cleanDuration);
+        }
+    }
+
+}
