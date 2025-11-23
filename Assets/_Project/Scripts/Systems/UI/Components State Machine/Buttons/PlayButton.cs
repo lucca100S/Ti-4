6578fc9bfe8cacb548004dc7afa94ba2590511ab
Arthur Roque <arthur.roque.pro@gmail.com>
2025-11-23 14:56:31 -0300

    Add main UI Canvas prefab and update UI logic
    Added a new Canvas.prefab for the main UI, including panels for general settings, credits, and game conclusion. Updated Arthur.unity scene and PlayButton.cs to integrate with the new UI structure.

--- Assets/_Project/Scripts/Systems/UI/Components State Machine/Buttons/PlayButton.cs
+++ Assets/_Project/Scripts/Systems/UI/Components State Machine/Buttons/PlayButton.cs
@@ -5,5 +5,6 @@ public class PlayButton : Button
     public override void OnClik()
     {
         FindAnyObjectByType<PlayerStartAnimationHandler>().StartAnimation();
+        FindAnyObjectByType<MainMenu>().OnExit(FindAnyObjectByType<HUD>());
     }
 }
