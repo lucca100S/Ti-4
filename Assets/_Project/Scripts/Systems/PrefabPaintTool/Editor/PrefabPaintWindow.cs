873e50b6ce53c7408574ff0f5b9d985c46fd5e72
LuguGMD <lugo.bhz@gmail.com>
2025-12-07 16:08:50 -0300

    style - Added Grass Blades

--- Assets/_Project/Scripts/Systems/PrefabPaintTool/Editor/PrefabPaintWindow.cs
+++ Assets/_Project/Scripts/Systems/PrefabPaintTool/Editor/PrefabPaintWindow.cs
@@ -5,36 +5,60 @@ using UnityEngine;
 
 public class PrefabPaintWindow : EditorWindow
 {
+    [SerializeField] private GameObject _groupObject;
+
     [SerializeField] private float _brushSize = 1.0f;
     [SerializeField] private GameObject _prefabToPaint;
     [SerializeField] private int _prefabMinCount = 1;
     [SerializeField] private int _prefabMaxCount = 5;
+
     [SerializeField] private int _layerMask = 0;
 
+    [SerializeField] private float _rotationVariance = 30f;
+    [SerializeField] private float _scaleVariance = 0.1f;
+
+    [SerializeField] private float _angleTreshold = 90f;
+
     private SerializedObject _paintWindowSO;
+
+    private SerializedProperty _groupObjectProp;
+
     private SerializedProperty _brushSizeProp;
     private SerializedProperty _prefabToPaintProp;
     private SerializedProperty _prefabMinCountProp;
     private SerializedProperty _prefabMaxCountProp;
     private SerializedProperty _layerMaskProp;
 
+    private SerializedProperty _rotationVarianceProp;
+    private SerializedProperty _scaleVarianceProp;
+
+    private SerializedProperty _angleTresholdProp;
+
     private const float RAYCAST_HEIGHT_OFFSET = 3f;
     private const float RAYCAST_MAX_DISTANCE = 10f;
 
-    void OnEnable() 
-    { 
+    void OnEnable()
+    {
         SceneView.duringSceneGui += this.OnSceneGUI;
 
         _paintWindowSO = new SerializedObject(this);
+
+        _groupObjectProp = _paintWindowSO.FindProperty("_groupObject");
+
         _brushSizeProp = _paintWindowSO.FindProperty("_brushSize");
         _prefabToPaintProp = _paintWindowSO.FindProperty("_prefabToPaint");
         _prefabMinCountProp = _paintWindowSO.FindProperty("_prefabMinCount");
         _prefabMaxCountProp = _paintWindowSO.FindProperty("_prefabMaxCount");
         _layerMaskProp = _paintWindowSO.FindProperty("_layerMask");
+
+        _rotationVarianceProp = _paintWindowSO.FindProperty("_rotationVariance");
+        _scaleVarianceProp = _paintWindowSO.FindProperty("_scaleVariance");
+
+        _angleTresholdProp = _paintWindowSO.FindProperty("_angleTreshold");
     }
-    void OnDisable() 
-    { 
-        SceneView.duringSceneGui -= this.OnSceneGUI; 
+    void OnDisable()
+    {
+        SceneView.duringSceneGui -= this.OnSceneGUI;
     }
 
     [MenuItem("Tools/Lugu/Prefab Paint #&P")]
@@ -49,8 +73,10 @@ public class PrefabPaintWindow : EditorWindow
     {
         _paintWindowSO.Update();
 
-        _brushSizeProp.floatValue = EditorGUILayout.FloatField("Brush Size", _brushSize);
+        _groupObjectProp.objectReferenceValue = (GameObject)EditorGUILayout.ObjectField("Parent Object", _groupObject, typeof(GameObject), true);
         _prefabToPaintProp.objectReferenceValue = (GameObject)EditorGUILayout.ObjectField("Prefab to Paint", _prefabToPaint, typeof(GameObject), false);
+
+        _brushSizeProp.floatValue = EditorGUILayout.FloatField("Brush Size", _brushSize);
 
         Rect rect = GUILayoutUtility.GetRect(0, 20);
         //_layerMaskProp.intValue = EditorGUI.LayerField(rect, "Layer Mask", _layerMask); // Placeholder for Layer Mask field
@@ -60,6 +86,11 @@ public class PrefabPaintWindow : EditorWindow
         _prefabMinCountProp.intValue = EditorGUILayout.IntField("Min", _prefabMinCount);
         _prefabMaxCountProp.intValue = EditorGUILayout.IntField("Max", _prefabMaxCount);
         EditorGUILayout.EndHorizontal();
+
+        _rotationVarianceProp.floatValue = EditorGUILayout.FloatField("Rotation Variance", _rotationVariance);
+        _scaleVarianceProp.floatValue = EditorGUILayout.FloatField("Scale Variance", _scaleVariance);
+
+        _angleTresholdProp.floatValue = EditorGUILayout.FloatField("Angle Treshold", _angleTreshold);
 
         _paintWindowSO.ApplyModifiedProperties();
     }
@@ -112,6 +143,11 @@ public class PrefabPaintWindow : EditorWindow
 
     private void PlaceAllPrefabs(Vector3 point, Vector3 normal)
     {
+        if (_groupObject == null)
+        {
+            CreateGroupObject();
+        }
+
         int prefabCount = Random.Range(Mathf.RoundToInt(_prefabMinCount), Mathf.RoundToInt(_prefabMaxCount) + 1);
         for (int i = 0; i < prefabCount; i++)
         {
@@ -127,6 +163,15 @@ public class PrefabPaintWindow : EditorWindow
             GameObject newObj = (GameObject)PrefabUtility.InstantiatePrefab(_prefabToPaint);
             newObj.transform.position = point;
             newObj.transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
+
+            float randomYRotation = Random.Range(-_rotationVariance, _rotationVariance);
+            newObj.transform.Rotate(Vector3.up, randomYRotation, Space.Self);
+
+            float randomScaleFactor = 1 + Random.Range(-_scaleVariance, _scaleVariance);
+            newObj.transform.localScale = newObj.transform.localScale * randomScaleFactor;
+
+            newObj.transform.parent = _groupObject.transform;
+
             Undo.RegisterCreatedObjectUndo(newObj, "Paint Prefab");
         }
     }
@@ -153,11 +198,22 @@ public class PrefabPaintWindow : EditorWindow
     private void GetFixedPosition(Vector3 point, Vector3 normal)
     {
         Ray ray = new Ray(point + normal * RAYCAST_HEIGHT_OFFSET, -normal);
-        //Debug.Log("Raycast from: " + ray.origin);
+
         if (Physics.Raycast(ray, out RaycastHit hit, RAYCAST_MAX_DISTANCE))
         {
-            
-            PlacePrefabAtPoint(hit.point, hit.normal);
+            if (Vector3.Dot(Vector3.up, hit.normal) >= Mathf.Sin(_angleTreshold))
+            {
+                PlacePrefabAtPoint(hit.point, hit.normal);
+            }
         }
+    }
+
+    private void CreateGroupObject()
+    {
+        _paintWindowSO.Update();
+        GameObject group = new GameObject("PaintedPrefabs_Group");
+        _groupObjectProp.objectReferenceValue = group;
+        _paintWindowSO.ApplyModifiedProperties();
+        Undo.RegisterCreatedObjectUndo(group, "Paint Group Object");
     }
 }
